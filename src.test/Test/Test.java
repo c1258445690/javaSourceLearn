5d85b9ec55df5735c27f7501290d9e913318460b
c1258445690 <1258445690@qq.com>
2020-03-31 16:08:48 +0800

    初始化

---
+++ src.test/Test/Test.java
@@ -0,0 +1,14 @@
+package Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+    public static void main(String[] args) {
+        Map<String,String> map=new HashMap<>();
+
+        map.put("abc","123");
+        map.put("de","456");
+        map.put("fgh","7890");
+    }
+}
